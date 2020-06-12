3af3ed0a27ac876764a012f5c51336ea995c4df1
Nezihe Merve Gürel <nezihemervegurel@gmail.com>
2020-06-12 16:49:08 +0200

    Create cluster-down.py

---
+++ dev/cluster-down.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+import paramiko
+import sys
+
+print(os.environ.keys())
+
+SCHEDULER_HOST = os.environ.get("SCHEDULER_HOST", None)
+if not SCHEDULER_HOST:
+    raise ValueError("The variable SCHEDULER_HOST not defined.")
+print("SCHEDULER_HOST=%s" % SCHEDULER_HOST)
+
+WORKER_HOSTS = os.environ.get("WORKER_HOSTS", None)
+if not WORKER_HOSTS:
+    raise ValueError("The variable WORKER_HOSTS not defined.")
+print("WORKER_HOSTS=%s" % WORKER_HOSTS)
+
+SSH_WORKINGDIR = os.environ.get("SSH_WORKINGDIR", None)
+if not SSH_WORKINGDIR:
+    raise ValueError("The variable SSH_WORKINGDIR not defined.")
+print("SSH_WORKINGDIR=%s" % SSH_WORKINGDIR)
+
+SSH_USERNAME = os.environ.get("SSH_USERNAME", None)
+if not SSH_USERNAME:
+    raise ValueError("The variable SSH_USERNAME not defined.")
+print("SSH_USERNAME=%s" % SSH_USERNAME)
+
+SSH_PASSWORD = os.environ.get("SSH_PASSWORD", None)
+
+# Start scheduler.
+print("Stopping scheduler on: %s" % SCHEDULER_HOST)
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.load_system_host_keys()
+client.connect(SCHEDULER_HOST, username=SSH_USERNAME, password=SSH_PASSWORD)
+stdin, stdout, stderr = client.exec_command("bash %s" % os.path.join(SSH_WORKINGDIR, "dev/src/stop-dask-scheduler"))
+print(stdout.read().decode())
+err = stderr.read().decode()
+if err:
+    print(err)
+    sys.exit(-1)
+
+# Start the workers.
+for WORKER_HOST in WORKER_HOSTS.split(","):
+    WORKER_HOST.strip()
+
+    print("Stopping worker on: %s" % WORKER_HOST)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.load_system_host_keys()
+    client.connect(WORKER_HOST, username=SSH_USERNAME, password=SSH_PASSWORD)
+    stdin, stdout, stderr = client.exec_command("bash %s" % os.path.join(SSH_WORKINGDIR, "dev/src/stop-dask-worker"))
+    print(stdout.read().decode())
+    err = stderr.read().decode()
+    if err:
+        print(err)
+        sys.exit(-1)
